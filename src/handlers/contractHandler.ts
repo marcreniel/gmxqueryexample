7b9a26caac6dda685b77f85690c00b63d202bdbf
imedra <marcbernardino2005@gmail.com>
2023-06-21 15:12:23 -0500

    migrated handler from ethers to alchemy-sdk

--- src/handlers/contractHandler.ts
+++ src/handlers/contractHandler.ts
@@ -1,23 +1,27 @@
-import { ethers, Log } from 'ethers'
+import { Alchemy, Network } from 'alchemy-sdk'
 //Import req modules
 
 //Connecting to Arbitrum Mainnet through Alchemy 
-const alchemy = `https://arb-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`;
-const contractAddress = '0x489ee077994B6658eAfA855C308275EAd8097C4A';
-const provider = new ethers.JsonRpcProvider(alchemy);
+const config = {
+    apiKey: process.env.ALCHEMY_API_KEY,
+    network: Network.ARB_MAINNET,
+}
 
 export default async function logContractFunctionCalls(topics: string | null) {
     try {
+        const alchemy = new Alchemy(config);
         // Querying recent logs using GMX contract, filtering using topic0 (function call in contract)
-        const logs = await provider.getLogs({
-            address: contractAddress,
+        let filters = {
+            address: "0x489ee077994B6658eAfA855C308275EAd8097C4A",
+            fromBlock: await alchemy.core.getBlockNumber() - 5000, 
+            toBlock: 'latest',
             topics: [topics],
-            fromBlock: await provider.getBlockNumber() - 5000, 
-            toBlock: 'latest', 
-        });
+        }
+
+        const logs = await alchemy.core.getLogs(filters);
 
         // Splits log into individual JSON 
-        const logArrayJSON = logs.map((log: Log) => {
+        const logArrayJSON = logs.map((log) => {
             return {
                 transactionHash: log.transactionHash,
                 blockNumber: log.blockNumber,
